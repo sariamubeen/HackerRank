7a6eead9a3ea57bb0dfd4f0986d4e6776981f550
Saria Mubeen <84678833+sariamubeen@users.noreply.github.com>
2024-01-02 16:16:46 +0500

    Regex Applications

---
+++ Regex/Applications/Saying Hi.py
@@ -0,0 +1,10 @@
+#Follow : https://github.com/sariamubeen
+import re
+
+if __name__ == '__main__':
+    n = int(input())
+    r = r'^[H|h][I|i]\s[^D|d]'
+    for _ in range(n):
+        s = input()
+        if re.match(r, s):
+            print(s)
