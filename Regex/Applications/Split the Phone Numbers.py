cf10de8b2b74cab261c031a8a9b839b894e2cd87
Saria Mubeen <84678833+sariamubeen@users.noreply.github.com>
2024-01-02 16:08:39 +0500

    Regex Applications

---
+++ Regex/Applications/Split the Phone Numbers.py
@@ -0,0 +1,10 @@
+#Follow : https://github.com/sariamubeen
+import re
+
+if __name__ == '__main__':
+    n = int(input())
+    Regex = r'(\d+)'
+    for _ in range(n):
+        s = input()
+        a = re.findall(Regex, s)
+        print('CountryCode=%s,LocalAreaCode=%s,Number=%s' %(str(a[0]), str(a[1]), str(a[2])))
