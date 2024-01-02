19a51781929beeac57e3c6ca9592a7228c1a11aa
Saria Mubeen <84678833+sariamubeen@users.noreply.github.com>
2024-01-02 16:06:31 +0500

    Regex Applications

---
+++ Regex/Applications/Find a Word.py
@@ -0,0 +1,16 @@
+#Follow : https://github.com/sariamubeen
+import re
+
+if __name__ == '__main__':
+    n = int(input())
+    s = [input() for _ in range(n)]
+    
+    t = int(input())
+    for _ in range(t):
+        ss = input()
+        Regex = '\\b' + ss + '\\b'
+        total = 0
+        for i in s:
+            words = re.findall(Regex, i)
+            total += len(words)
+        print(total)
