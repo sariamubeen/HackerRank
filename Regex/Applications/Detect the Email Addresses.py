a60edcbd2c807af358d36f8bcfa25511a96ba5f5
Saria Mubeen <84678833+sariamubeen@users.noreply.github.com>
2024-01-02 16:06:52 +0500

    Regex Applications

---
+++ Regex/Applications/Detect the Email Addresses.py
@@ -0,0 +1,15 @@
+#Follow : https://github.com/sariamubeen
+import re
+
+if __name__ == '__main__':
+    n = int(input())
+    ls = set()
+    Regex = r'\b(?:\w|\.){1,}@(?:\w|\.*){1,}\b'
+    for _ in range(n):
+        s = input()
+        found = re.findall(Regex, s)
+        for i in found:
+            if i not in ls:
+                ls.add(i)
+                
+    print(';'.join(sorted(ls)))
