cdd32e5451cf4a415b408d941ac16c98076a24d3
Saria Mubeen <84678833+sariamubeen@users.noreply.github.com>
2024-01-02 16:13:44 +0500

    Regex Applications

---
+++ Regex/Applications/Utopian Identification Number.py
@@ -0,0 +1,12 @@
+#Follow : https://github.com/sariamubeen
+import re
+
+if __name__ == '__main__':
+    n = int(input())
+    Regex = r'^[a-z]{0,3}[0-9]{2,8}[A-Z]{3,}'
+    for _ in range(n):
+        s = input()
+        if re.match(Regex, s):
+            print('VALID')
+        else:
+            print('INVALID')
