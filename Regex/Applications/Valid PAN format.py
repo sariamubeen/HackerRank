5b221e5bfa397744f32b31d99150dc425e92c5f9
Saria Mubeen <84678833+sariamubeen@users.noreply.github.com>
2024-01-02 16:14:14 +0500

    Regex Applications

---
+++ Regex/Applications/Valid PAN format.py
@@ -0,0 +1,12 @@
+#Follow : https://github.com/sariamubeen
+import re
+
+if __name__ == '__main__':
+    n = int(input())
+    Regex = r'[A-Z]{5}[0-9]{4}[A-Z]{1}'
+    for _ in range(n):
+        s = input()
+        if re.match(Regex, s):
+            print('YES')
+        else:
+            print('NO')
