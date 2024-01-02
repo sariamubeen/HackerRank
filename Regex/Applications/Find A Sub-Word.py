a5d853a35f79c8cfad75edf626dacf1f0bef6ea5
Saria Mubeen <84678833+sariamubeen@users.noreply.github.com>
2024-01-02 16:03:58 +0500

    Regex Applications

---
+++ Regex/Applications/Find A Sub-Word.py
@@ -0,0 +1,12 @@
+#Follow : https://github.com/sariamubeen
+import re
+
+if __name__ == '__main__':
+    n = int(input())
+    nline = '\n'.join(input() for _ in range(n))
+        
+    q = int(input())
+    
+    for _ in range(q):
+        s = input()
+        print(len(re.findall(r'\B(%s)\B' % s, nline)))
