1489c2202cc8863b5c6a7dd9f222e050f0e0da6f
Saria Mubeen <84678833+sariamubeen@users.noreply.github.com>
2024-01-02 16:09:21 +0500

    Regex Applications

---
+++ Regex/Applications/Building a Smart IDE: Programming Language Detection.py
@@ -0,0 +1,19 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+import sys
+
+class Main:
+    def __init__(self):
+        self.s = ''.join(sys.stdin.readlines())
+        
+    def output(self):
+        if 'java' in self.s:
+            print("Java")
+        elif '#include' in self.s:
+            print("C")
+        else:
+            print("Python")
+        
+if __name__ == '__main__':
+    obj = Main()
+    obj.output()
