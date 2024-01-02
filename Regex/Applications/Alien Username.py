b802d4d5d4e00b1a539558e93db23496e2073e9c
Saria Mubeen <84678833+sariamubeen@users.noreply.github.com>
2024-01-02 16:04:33 +0500

    Regex Applications

---
+++ Regex/Applications/Alien Username.py
@@ -0,0 +1,12 @@
+#Follow : https://github.com/sariamubeen
+import re
+
+if __name__ == '__main__':
+    n = int(input())
+    r = r'^[_.]\d+[a-z|A-Z]*[_]?$'
+    for _ in range(n):
+        s = input()
+        if re.match(r, s):
+            print('VALID')
+        else:
+            print('INVALID')
