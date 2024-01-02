de579d5694807c28c48ad7603a3c272c90b6d343
Saria Mubeen <84678833+sariamubeen@users.noreply.github.com>
2024-01-02 16:22:27 +0500

    Regex Applications

---
+++ Regex/Applications/HackerRank Tweets.py
@@ -0,0 +1,12 @@
+#Follow : https://github.com/sariamubeen
+import re
+
+if __name__ == '__main__':
+    n = int(input())
+    con = 0
+    for _ in range(n):
+        s = input().upper()
+        if 'HACKERRANK' in s:
+            con += 1
+            
+    print(con)
